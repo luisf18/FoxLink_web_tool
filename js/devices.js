b44e915dc65918eedc10e1428dd242ecff3e4b7e
luisf18 <luisfelipefonte@gmail.com>
2026-02-08 08:09:05 -0300

    adição do módulo esc brushed duplo, ainda faltam ajustes

--- js/devices.js
+++ js/devices.js
@@ -4,8 +4,8 @@
 
 export const deviceRegistry = {
     0x001: { name: "FX_S50", type: "sensor" },
-    0x020: { name: "IR_KEY", type: "sensor" }
-    //0x200: { name: "BESC8p", type: "ESC brushed" }
+    0x020: { name: "IR_KEY", type: "sensor" },
+    0x200: { name: "BESC8p_double", type: "ESC brushed" }
 };
 
 export const deviceDefs = {
@@ -113,6 +113,164 @@ export const deviceDefs = {
                         type: "check",
                         defaultValue: 0, // digital
                         checkOptions: ["Log enable"]
+                    }
+                }
+            }
+        }
+    },
+
+    BESC8p_double: {
+        base: {
+            model: "ESC 2x Brushed",
+            image: "devices/besc8p.png",
+            //graphOptions: {
+            //    points: 60,
+            //    autoScale: true,
+            //    min: 0,
+            //    max: 1,
+            //    hideLegend: true,
+            //    hideYAxis: true,
+            //    variables: [ { name: "read", color: "orange" } ]
+            //},
+            param: {
+                "name": {
+                    addr: 47,
+                    wg: {
+                        name: "Name",
+                        type: "string",
+                        defaultValue: "BESC 2x",
+                        outputLen: 16
+                    }
+                },
+                "basic": {
+                    addr: 0x03,
+                    wg: {
+                        name: "Basic config.", type: "check",
+                        defaultValue: 0, // digital
+                        checkOptions: [ "Mix", "swap channels" ]
+                    }
+                },
+                "failsafe": {
+                    addr: 0x07,
+                    wg: {
+                        name: "Failsafe delay", type: "slide",
+                        defaultValue: 63,
+                        abstract: [148, 4*255]
+                    }
+                },
+                // Motor 1 =================================
+                "M1_config": {
+                    addr: 11,
+                    wg: {
+                        name: "Motor1 config.", type: "check",
+                        defaultValue: 0, // digital
+                        checkOptions: [ "Bidirectional", "Invert", "limit Min", "limit Max" ]
+                    }
+                },
+                "M1_min": {
+                    addr: 12,
+                    wg: {
+                        name: "M1 min power", type: "slide",
+                        defaultValue: 0,
+                        abstract: [0, 100]
+                    }
+                },
+                "M1_max": {
+                    addr: 13,
+                    wg: {
+                        name: "M1 max power", type: "slide",
+                        defaultValue: 255,
+                        abstract: [0, 100]
+                    }
+                },
+                "M1_ch_center": {
+                    addr: 14,
+                    wg: {
+                        name: "M1 ch center", type: "slide",
+                        defaultValue: 255,
+                        abstract: [1000, 2000]
+                    }
+                },
+                "M1_ch_min": {
+                    addr: 15,
+                    wg: {
+                        name: "M1 ch min", type: "slide",
+                        defaultValue: 255,
+                        abstract: [1000, 2000]
+                    }
+                },
+                "M1_ch_max": {
+                    addr: 16,
+                    wg: {
+                        name: "M1 ch max", type: "slide",
+                        defaultValue: 255,
+                        abstract: [1000, 2000]
+                    }
+                },
+                "M1_ch_deadband": {
+                    addr: 17,
+                    wg: {
+                        name: "M1 ch deadband", type: "slide",
+                        defaultValue: 255,
+                        abstract: [1000, 2000]
+                    }
+                },
+
+                // Motor 2 =================================
+                "M2_config": {
+                    addr: 20,
+                    wg: {
+                        name: "Motor2 config.", type: "check",
+                        defaultValue: 0, // digital
+                        checkOptions: [ "Bidirectional", "Invert", "limit Min", "limit Max" ]
+                    }
+                },
+                "M2_min": {
+                    addr: 21,
+                    wg: {
+                        name: "M2 min power", type: "slide",
+                        defaultValue: 0,
+                        abstract: [0, 100]
+                    }
+                },
+                "M2_max": {
+                    addr: 22,
+                    wg: {
+                        name: "M2 max power", type: "slide",
+                        defaultValue: 255,
+                        abstract: [0, 100]
+                    }
+                },
+                "M2_ch_center": {
+                    addr: 23,
+                    wg: {
+                        name: "M2 ch center", type: "slide",
+                        defaultValue: 255,
+                        abstract: [1000, 2000]
+                    }
+                },
+                "M2_ch_min": {
+                    addr: 24,
+                    wg: {
+                        name: "M2 ch min", type: "slide",
+                        defaultValue: 255,
+                        abstract: [1000, 2000]
+                    }
+                },
+                "M2_ch_max": {
+                    addr: 25,
+                    wg: {
+                        name: "M2 ch max", type: "slide",
+                        defaultValue: 255,
+                        abstract: [1000, 2000]
+                    }
+                },
+                "M2_ch_deadband": {
+                    addr: 26,
+                    wg: {
+                        name: "M2 ch deadband", type: "slide",
+                        defaultValue: 255,
+                        abstract: [1000, 2000]
                     }
                 }
             }
